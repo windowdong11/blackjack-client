28909434c2724a2976e8ab037d6ea53bfda382ee
WonDongGyu <windowdong11@naver.com>
2020-09-13 01:50:48 +0900

    SignIn, SignUp 페이지 라우팅 추가 및 links 코드분리 반영

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import styled, { createGlobalStyle } from 'styled-components';
+import NavigationBar from './components/NavigationBar';
+import links from './Links';
+import SignIn from './pages/Auth/SignIn';
+import SignUp from './pages/Auth/SignUp';
 
-import NavigationBar from './components/NavigationBar'
-import { createGlobalStyle } from 'styled-components';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const GlobalStyle = createGlobalStyle`
+  html, body, body>div#root{
+    width: 100%;
+    height: 100%;
+  }
   body {
     padding: 0;
     margin: 0;
@@ -13,35 +20,38 @@ const GlobalStyle = createGlobalStyle`
     text-decoration: none;
   }
 `
+const ContentWrapper = styled.div`
+  background-color: black;
+  width: 100%;
+  height: 100%;
+`
 
 
 function App() {
   return (
-    <div className="App">
+    <ContentWrapper>
       <GlobalStyle />
-      <header className="App-header">
-        <Router>
-          <NavigationBar/>
-          <Switch>
-            <Route exact path="/">
-              Home
+      <BrowserRouter>
+        <NavigationBar />
+        <Switch>
+          <Route exact path="/">
+            Home
             </Route>
-            <Route exact path="/login">
-              Login
+          <Route exact path={links.signin}>
+            <SignIn />
+          </Route>
+          <Route exact path={links.signup}>
+            <SignUp />
+          </Route>
+          <Route exact path={links.payment}>
+            Pay
             </Route>
-            <Route exact path="/logout">
-              Logout
+          <Route>
+            Nomatch
             </Route>
-            <Route exact path="/payment">
-              Pay
-            </Route>
-            <Route>
-              Nomatch
-            </Route>
-          </Switch>
-        </Router>
-      </header>
-    </div>
+        </Switch>
+      </BrowserRouter>
+    </ContentWrapper>
   );
 }
 
