54aac0b7df8f89b4b522977d24ec85213232cbb6
WonDongGyu <windowdong11@naver.com>
2020-09-12 11:54:44 +0900

    Update NavigationBar style

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import links from '../Links'
 
 
 const Nav = styled.nav`
-    background-color: black;
+    background: linear-gradient(0deg, rgba(0,0,0, 1)-3%, rgba(0,0,0,0.1) 20%, transparent 35%),
+                linear-gradient(190deg,black -10%,rgb(38 167 210 / 44%) 10%,rgba(15,24,27,0.5) 39%,rgba(20,10,10,0.2) 62%,rgba(30,10,10,0.5) 90%,black 100%);
     color: chocolate;
     display:flex;
     align-items: center;
@@ -14,7 +16,7 @@ const Title = styled(Link)`
     font-family: fantasy;
     font-size: xxx-large;
     margin: 8px 12px;
-    color: inherit;
+    color: chocolate;
 `
 
 const PageList = styled.ul`
@@ -27,7 +29,7 @@ const PageItem = styled.li`
 `
 
 const PageItemLink = styled(Link)`
-    color: inherit;
+    color: rgba(210,270,250,0.7);
 `
 
 export default ((props) => {
@@ -35,8 +37,9 @@ export default ((props) => {
         <Nav>
             <Title to="/">Blackjack</Title>
             <PageList>
-                <PageItem><PageItemLink to="/login">Login</PageItemLink></PageItem>
-                <PageItem><PageItemLink to="/logout">Logout</PageItemLink></PageItem>
+                <PageItem><PageItemLink to={links.signin}>Signin</PageItemLink></PageItem>
+                <PageItem><PageItemLink to={links.signout}>SignOut</PageItemLink></PageItem>
+                <PageItem><PageItemLink to={links.signup}>SignUp</PageItemLink></PageItem>
             </PageList>
         </Nav>
     )
