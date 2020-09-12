12748a308ecaedbd1bf19048422c6f007193beca
WonDongGyu <windowdong11@naver.com>
2020-09-13 01:45:47 +0900

    SignIn, SignUp페이지 등에 사용할 AuthButton Component 추가

---
+++ src/components/Auth/AuthButton.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button`
+    width: 100%;
+    border: none;
+    outline: none;
+
+    line-height: 2.5rem;
+    font-size: 1.2rem;
+
+    margin-top: 1rem;
+
+    padding-left : 0.5rem;
+    padding-right : 0.5rem;
+
+    background : linear-gradient(to right,#9c27b078,#673ab7f0);
+
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+
+    width: 80%;
+    
+    text-align: center;
+`
+
+export default ({onClick, children, ...rest}) => {
+    return(
+        <Button onClick={onClick} {...rest}>{children}</Button>
+    )
+}
