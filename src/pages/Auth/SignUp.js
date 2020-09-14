c4304a9526ea5f0cd156688715ae0f065d6931aa
WonDongGyu <windowdong11@naver.com>
2020-09-14 23:23:09 +0900

    SignUp 컴포넌트 클래스형에서 함수형으로 변경

--- src/pages/Auth/SignUp.js
+++ src/pages/Auth/SignUp.js
@@ -1,30 +1,35 @@
-import { useQuery } from '@apollo/client'
 import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
 import AuthButton from '../../components/Auth/AuthButton'
 import AuthWrapper from '../../components/Auth/AuthWrapper'
 import InputwithLabel from '../../components/InputwithLabel'
 import { REGISTER } from '../../httprequests/mutation'
 
-export default class SignUp extends React.Component {
-    constructor(props) {
-        super()
-        this.state = {
-            id: '',
-            pw: ''
-        }
+export default function SignUp(props) {
+    const [id, setId] = useState('')
+    const [pw, setPw] = useState('')
+    
+    
+    const [register, { data }] = useMutation(REGISTER)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(id)
+        console.log(pw)
+        console.log(data)
+        register({variables: { id: id, pw: pw }}).catch(error => console.log(error));
+        console.log(data)
     }
-    handleInputChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value })
-    }
-    render() {
-        return (
-            <AuthWrapper Title="Sign Up">
+
+    return (
+        <AuthWrapper Title="Sign Up">
+            <form onSubmit={handleSubmit}>
                 <InputwithLabel name="id" label="ID"
-                    onChange={this.handleInputChange} value={this.state.id} />
+                    onChange={e => setId(e.target.value)} value={id} />
                 <InputwithLabel name="pw" label="PW"
-                    onChange={this.handleInputChange} value={this.state.pw} />
-                <AuthButton onClick={this.handleClick}>Sign Up</AuthButton>
-            </AuthWrapper>
-        )
-    }
+                    onChange={e => setPw(e.target.value)} value={pw} />
+                <AuthButton type="submit">Sign Up</AuthButton>
+            </form>
+        </AuthWrapper>
+    )
 }
