b5188c41ab2b9026f6a5f920b1a284e5a243841e
WonDongGyu <windowdong11@naver.com>
2020-09-17 21:48:27 +0900

    로그아웃 기능 구현

---
+++ src/pages/Auth/Logout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useMutation } from '@apollo/client'
+import { LOGOUT } from '../../httprequests/mutation'
+import { Redirect } from 'react-router-dom'
+import { removeCookie } from '../../components/cookie/Cookie'
+
+
+export default () => {
+    const { loading, error} = useMutation(LOGOUT)
+    if(loading) return 'Loading...';
+    if(error) return `Error! ${error.message}`
+    removeCookie('token')
+    return(
+        <Redirect to="/"></Redirect>
+    )
+}
