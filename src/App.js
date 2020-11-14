fd40cd5616099430ca93699a7eaa4c5cf0fd16bc
WonDongGyu <windowdong11@naver.com>
2020-11-15 00:49:28 +0900

    네비게이션 바 메뉴에 마우스 올리면 화면 커졌다 작아졌다 하는 문제 해결
    hover에만 padding을 주는게 아니라 기본 상태에서도 padding을 줌

--- src/App.js
+++ src/App.js
@@ -19,17 +19,34 @@ import playingDesk_1920 from './images/playingdesk_1920.jpg'
 import cards_1280 from './images/cards_1280.png'
 import cards_svg from './images/cards.svg'
 
+
+const FitImg = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+`
+// ------------------------ Navigation Bar ---------------------------
+
 const NavBar = styled.div`
   /* background-color: rgba(136, 114, 243, 1); */
   height: 3em;
+  min-width : fit-content;
+
   padding: 0.5em;
+
   display: flex;
   align-items: center;
   justify-content: space-between;
-  min-width : fit-content;
+  
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: calc(100% - 1em);
+  outline: solid black 2px;
+
+  background-color: white;
 `
 
-const NavLogo = styled.div`
+const NavLogo = styled(Link)`
   height: inherit;
 `
 
@@ -41,13 +58,13 @@ const NavMenu = styled.ul`
     
   }
   li{
+    padding-top: 1em;
+    padding-bottom: 1em;
+    padding-left: 12px;
+    padding-right: 12px;
     :hover{
       color: white;
       background-color: black;
-      padding-top: 1em;
-      padding-bottom: 1em;
-      padding-left: 12px;
-      padding-right: 12px;
     }
   }
   list-style: none;
@@ -65,66 +82,97 @@ const NavAuth = styled.div`
 display: flex;
 flex-direction: column;
 `
-
+// ------------------------ Body ---------------------------
 const BodyWrapper = styled.div`
   /* background-color: rgba(158, 159, 214, 1); */
   padding: 0;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  outline: solid black 2px;
-`
-const ContentWrapper = styled.div`
-  width: 80%;
+  padding-top: 1em;
+  margin-top: 4em;
+
+  min-height: calc(100vh - 9em);/* 100% - 헤더 높이 - 푸터 높이 */
+
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const MainImg = styled(FitImg)`
+  display: block;
+
+  max-width: 55%;
+
+  margin-right: auto;
+  margin-left:auto;
+`
+
+const ContentWrapper = styled.div`
+  display: flex;
+
   justify-content: center;
   align-items: center;
-  outline: solid yellow 2px;
+
+  width: 100%;
+  
+  padding-bottom: 2em;
+
+  background-color: #0b090c;
+  color : rgb(177 127 144);
+  font-size: large;
+  font-weight: bold;
 `
 
-const FitImg = styled.img`
-  max-width: 100%;
-  max-height: 100%;
+// -------------------- Footer -------------------
+
+const FootWrapper = styled.footer`
+  /* position: fixed;
+  left: 0;
+  bottom: 0; */
+  height: 3em;
+  width: calc(100% - 1em); /* 100% - padding(left + right) */
+
+  padding: 0.5em;
+
+  background-color: #282c34;
+
+  color: #ffffff80;
 `
-
-function CustomNav(props){
-  const auth = useAuth()
-
-}
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <ProvideAuth>
         <BrowserRouter>
-        <NavBar>
-          <NavLogo>
-            <FitImg src={cards_svg}></FitImg>
-          </NavLogo>
-          <NavMenu>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/chat'>Chat</Link></li>
-            <li><Link to='/howto'>HowTo</Link></li>
-          </NavMenu>
-          <NavAuth>
-            <NonePrivateLink to='/login'>Login</NonePrivateLink>
-            <NonePrivateLink to='/register'>Register</NonePrivateLink>
-            <LogoutButton>Logout</LogoutButton>
-          </NavAuth>
-        </NavBar>
-        <BodyWrapper>
-          <ContentWrapper>
-            <FitImg src={cards_1280}></FitImg>
-            <Route exact path='/'><Home></Home></Route>
-            <PrivateRoute path='/chat'><Chat/></PrivateRoute>
-            <NonePrivateRoute path='/login'><Login></Login></NonePrivateRoute>
-            <NonePrivateRoute path='/register'><Register></Register></NonePrivateRoute>
-          </ContentWrapper>
-        </BodyWrapper>
-        <div>
-          foot
-        </div>
+
+          <NavBar>
+            <NavLogo to='/'>
+              <FitImg src={cards_svg}></FitImg>
+            </NavLogo>
+            <NavMenu>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/chat'>Chat</Link></li>
+              <li><Link to='/howto'>HowTo</Link></li>
+            </NavMenu>
+            <NavAuth>
+              <NonePrivateLink to='/login'>Login</NonePrivateLink>
+              <NonePrivateLink to='/register'>Register</NonePrivateLink>
+              <LogoutButton>Logout</LogoutButton>
+            </NavAuth>
+          </NavBar>
+
+          <BodyWrapper>
+            <MainImg src={cards_1280}></MainImg>
+            <ContentWrapper>
+              <Route exact path='/'><Home></Home></Route>
+              <PrivateRoute path='/chat'><Chat /></PrivateRoute>
+              <NonePrivateRoute path='/login'><Login></Login></NonePrivateRoute>
+              <NonePrivateRoute path='/register'><Register></Register></NonePrivateRoute>
+            </ContentWrapper>
+          </BodyWrapper>
+
+          <FootWrapper>
+            BlackJack App
+          </FootWrapper>
         </BrowserRouter>
       </ProvideAuth>
     </ApolloProvider>
